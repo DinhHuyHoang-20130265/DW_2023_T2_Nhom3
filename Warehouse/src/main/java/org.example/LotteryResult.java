09855bd18cc531b1063a59827f4fab6bd2bcc00e
DinhHuyHoang-20130265 <20130265@st.hcmuaf.edu.vn>
2023-11-24 16:50:00 +0700

    Start create Modules and Class to Data Warehouse

---
+++ Warehouse/src/main/java/org.example/LotteryResult.java
@@ -0,0 +1,27 @@
+package org.example;
+
+import lombok.*;
+
+import java.util.List;
+
+@Getter
+@ToString
+@RequiredArgsConstructor
+public class LotteryResult {
+    @NonNull
+    @Setter
+    private String lotteryDomain;
+    @Setter
+    @NonNull
+    private String province;
+    @Setter
+    @NonNull
+    private String date;
+    @Setter
+    @NonNull
+    private String tenGiai;
+    @Setter
+    @NonNull
+    private String soTrungThuong;
+
+}
